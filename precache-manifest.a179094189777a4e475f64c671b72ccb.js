93284e96fb75ea827b9b7df45eb76d204631aeb7
rodneyslafuente <rodneyslafuente@gmail.com>
2020-03-02 07:50:38 -0500

    Updates

---
+++ precache-manifest.a179094189777a4e475f64c671b72ccb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a44eaad97cd944619f19d5ca0768e8fb",
+    "url": "%3CGITHUB_USERNAME%3E.github.io/%3CAPP_NAME%3E/index.html"
+  },
+  {
+    "revision": "6cf2e1fff89d57cacb0a",
+    "url": "%3CGITHUB_USERNAME%3E.github.io/%3CAPP_NAME%3E/static/js/2.7915df8f.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "%3CGITHUB_USERNAME%3E.github.io/%3CAPP_NAME%3E/static/js/2.7915df8f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f1a5b0cbd0345e6620b4",
+    "url": "%3CGITHUB_USERNAME%3E.github.io/%3CAPP_NAME%3E/static/js/main.b292be1f.chunk.js"
+  },
+  {
+    "revision": "0fa4e1034d0cce5d7835",
+    "url": "%3CGITHUB_USERNAME%3E.github.io/%3CAPP_NAME%3E/static/js/runtime-main.51614db5.js"
+  }
+]);
